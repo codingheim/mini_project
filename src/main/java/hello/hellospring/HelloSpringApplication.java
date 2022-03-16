38b3db43e4813330a5b09c9853fb01ee7429837b
codingheim <suyong0697@naver.com>
2022-03-16 23:49:16 +0900

    mini_project

---
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -0,0 +1,14 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+  public static void main(String[] args) {
+		System.out.println("hello");
+  	SpringApplication.run(HelloSpringApplication.class, args);
+  }
+
+}
